8672ace0e836459556852d3d2d2c0fc2a362efc8
kewalsingh <kewal5759@gmail.com>
2023-09-12 16:16:22 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+import LogIn from './android/app/component/LogIn'
+const App = () => {
+  return (
+    <View style={{flex:1}}>
+      <LogIn/>
+    
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
